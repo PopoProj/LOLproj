edd72c5d84f3828e0e9247dd40009437e935f9f7
slee <lsy5881@gmail.com>
2023-09-20 16:04:06 +0900

    new

--- miniProj/src/main/java/com/my/miniProj/MiniProjApplication.java
+++ miniProj/src/main/java/com/my/miniProj/MiniProjApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = {"com.my.miniProj.*"})
 public class MiniProjApplication {
 
 	public static void main(String[] args) {
