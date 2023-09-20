a187c01328d9aa6048fb4764ff85eb2dd5a6d81e
crong <chaperone506@gmail.com>
2023-09-20 16:32:32 +0900

    버전 맞추는 중

---
+++ miniProj/src/main/java/com/my/miniProj/model/BoardDao.java
@@ -0,0 +1,26 @@
+package com.my.miniProj.model;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardDao {
+	// 게시글 등록 처리
+		public void create(Board board) throws Exception;
+
+		// 게시글 목록 페이지
+		public List<Board> list(Pages pages) throws Exception;
+
+		// 게시글 상세 페이지
+		public Board read(Integer boardNum) throws Exception;
+
+		// 게시글 수정 처리
+		public void update(Board board) throws Exception;
+
+		// 게시글 삭제 처리
+		public void delete(Integer boardNum) throws Exception;
+		
+		// 게시글 전체 건수 반환
+		public int count() throws Exception;
+}
